a32b9873b5c00dbd5d2eea589a29bb2cf4d060fe
Sasa Radovanovic <salle90ar@gmail.com>
2020-07-08 10:22:58 +0200

    Title change

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 const About = () => {
   return (
-    <div>
+    <div className='text-light'>
       <h1>This is Github Finder App</h1>
-      <p className="lead">Version: 1.0.0</p>
+      <p className='lead'>Version: 1.0.0</p>
     </div>
   );
 };
